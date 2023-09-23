b113bedcd1d7dbce3175687d01d7c8af115225ca
tehnickge <71207932+tehnickge@users.noreply.github.com>
2023-09-23 10:39:40 +0300

    a
    a

--- front/app/src/components/Costs/CostItem.js
+++ front/app/src/components/Costs/CostItem.js
@@ -1,17 +1,27 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
+import React, { useState } from "react"
 
 const CostItem = (props) => {
+
+  const [description, setDescription] = useState(props.description);
+
+  const changeDescriptionHandler = (props) => {
+    setDescription('HUI');
+  }
+
   return (
     <Card className="cost-item">
       <div>
         <CostDate date={props.date} />
       </div>
       <div className="cost-item__description">
-        <h2>{props.description}</h2>
+        <h2>{description}</h2>
         <div className="cost-item__price">${props.amount}</div>
       </div>
+      <button onClick={changeDescriptionHandler}>изменить описание</button>
+
     </Card>
   );
 }
